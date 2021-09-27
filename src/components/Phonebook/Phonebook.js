7b37d76d43c34593994eebeeebc4927e6733f928
HPavlova <77845327+HPavlova@users.noreply.github.com>
2021-09-27 17:24:29 +0300

    commit init

---
+++ src/components/Phonebook/Phonebook.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+/* <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+  required
+/>; */
